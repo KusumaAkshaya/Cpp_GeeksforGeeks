fde574fbde7c8a63e3f5090a366cce634a3c1f73
KusumaAkshaya <sriramaabhayam@gmail.com>
2024-12-05 11:44:53 +0530

    method3: space optimization of method 2

--- comments.cpp
+++ comments.cpp
@@ -6,18 +6,17 @@ class GFC{
 public:
 int fib(int n)
 {
-    int f[n+2];
+    int a=0, b=1, c;
     int i;
-
-    f[0] = 0;
-    f[1] = 1;
 
     for(i=2; i<=n; i++)
     {
-        f[i] = f[i-1] + f[i-2];
+       c = a+b;
+       a = b;
+       b = c;
     }
 
-    return f[n];
+    return c;
 
 }
 };
