4358105bf9ef55a9a4343fb93620e5394d83b133
KusumaAkshaya <sriramaabhayam@gmail.com>
2024-12-05 13:28:17 +0530

    Method6: o(logn) time

--- comments.cpp
+++ comments.cpp
@@ -1,56 +1,34 @@
 #include <iostream>
 using namespace std;
 
-void multiply(int F[2][2], int M[2][2]);
+const int MAX = 1000;
 
-void power(int F[2][2], int n);
+int f[MAX] = {0};
 
 int fib(int n)
 {
-    int F[2][2] = {{1, 1}, {1, 0}};
+   if(n == 0)
+   return 0;
 
-    if(n == 0) return 0;
-    
-    power(F, n-1);
+   if(n == 1 || n ==2 ) return (f[n] = 1);
+   
+   if(f[n]) return f[n];
 
-    return F[0][0];
+   int k = (n & 1) ? (n+1)/2 : n/2; // n&1 is 1 if n is odd , else 0
 
-}
+    f[n] = (n & 1)? (fib(k)*fib(k) + fib(k-1)*fib(k-1))
+           : (2*fib(k-1) + fib(k))*fib(k);
 
-void multiply(int F[2][2], int M[2][2])
-{
-    int x = F[0][0]*M[0][0] + F[0][1]*M[1][0];
-    int y = F[0][0]*M[0][1] + F[0][1]*M[1][1];
-    int z = F[1][0]*M[0][0] + F[1][1]*M[1][0];
-    int w = F[1][0]*M[0][1] + F[1][1]*M[1][1];
-
-    F[0][0] = x;
-    F[0][1] = y;
-    F[1][0] = z;
-    F[1][1] = w;
+     return f[n];
 
 }
 
 
-void power(int F[2][2], int n)
-{
-    int i; 
-    if(n == 0 || n == 1) return;
-    
-    int M[2][2] = {{1, 1}, {1, 0}};
-
-    power(F, n/2);
-    multiply(F, F);
-
-    if(n % 2 != 0)
-    multiply(F, M);
-
-}
 
 int main()
 {
    int n ;
    cin >> n;
-   cout << "fibonacci( " << n << " ) : " << fib(n) << endl;
+   cout << "fibonacci(" << n << ") : " << fib(n) << endl;
    return 0;
 }
