0b1f580b360d160c7aa876a102513ea048f16ff1
KusumaAkshaya <sriramaabhayam@gmail.com>
2024-12-22 10:31:06 +0530

    Template

---
+++ stl.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<class T> T add( T x )
+{
+    return x+x;
+}
+
+int main()
+{
+    cout << add(6.78);
+    cout << add(3);
+    cout << add(string{"hello "});
+    
+}
